16dc5f8ad645c31caaf8830dad336a2c4e4ca9ec
abenteuerzeit <amroz.muzyka@gmail.com>
2022-09-08 07:27:32 +0200

    Implement find by Name

--- Indexers/MainPage.xaml.cs
+++ Indexers/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,7 +20,13 @@ namespace Indexers
 
         private void findByNameClick(object sender, RoutedEventArgs e)
         {
-            // TODO:
+            string text = name.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                Name name = new(text);
+                PhoneNumber number = this.phoneBook[name];
+                phoneNumber.Text = string.IsNullOrEmpty(number.Text) ? "Not Found" : number.Text;
+            }
         }
 
         private void findByPhoneNumberClick(object sender, RoutedEventArgs e)
