8055a1388139310ce90540f80a7c99af1a9a389d
abenteuerzeit <amroz.muzyka@gmail.com>
2022-09-08 07:07:07 +0200

    Find the name that matches the specified phone number

--- Indexers/PhoneBook.cs
+++ Indexers/PhoneBook.cs
@@ -2,47 +2,61 @@ using System;
 
 namespace Indexers
 {
-	sealed class PhoneBook
-	{
+    sealed class PhoneBook
+    {
         private int used;
         private Name[] names;
         private PhoneNumber[] phoneNumbers;
 
-		public PhoneBook()
-		{
-			int initialSize = 0;
-			this.used = 0;
-			this.names = new Name[initialSize];
-			this.phoneNumbers = new PhoneNumber[initialSize];
-		}
-		
-		public void Add(Name name, PhoneNumber number)
-		{
-			enlargeIfFull();
-			this.names[used] = name;
-			this.phoneNumbers[used] = number;
-			this.used++;
-		}
-		
-		// TODO: write 1st indexer here
+        public PhoneBook()
+        {
+            int initialSize = 0;
+            this.used = 0;
+            this.names = new Name[initialSize];
+            this.phoneNumbers = new PhoneNumber[initialSize];
+        }
 
-		// TODO: write 2nd indexer here
+        public void Add(Name name, PhoneNumber number)
+        {
+            enlargeIfFull();
+            this.names[used] = name;
+            this.phoneNumbers[used] = number;
+            this.used++;
+        }
 
-		private void enlargeIfFull()
-		{
-			if (this.used == this.names.Length)
-			{
-				int bigger = used + 16;
-				
-				Name[] moreNames = new Name[bigger];
-				this.names.CopyTo(moreNames, 0);
-				
-				PhoneNumber[] morePhoneNumbers = new PhoneNumber[bigger];
-				this.phoneNumbers.CopyTo(morePhoneNumbers, 0);
-						
-				this.names = moreNames;
-				this.phoneNumbers = morePhoneNumbers;
-			}
-		}
-	}
+        public Name this[PhoneNumber number]
+        {
+            get
+            {
+                int i = Array.IndexOf(this.phoneNumbers, number);
+                if (i != -1)
+                {
+                    return this.names[i];
+                }
+                else
+                {
+                    return new Name();
+                }
+            }
+        }
+
+        // TODO: write 2nd indexer here
+
+        private void enlargeIfFull()
+        {
+            if (this.used == this.names.Length)
+            {
+                int bigger = used + 16;
+
+                Name[] moreNames = new Name[bigger];
+                this.names.CopyTo(moreNames, 0);
+
+                PhoneNumber[] morePhoneNumbers = new PhoneNumber[bigger];
+                this.phoneNumbers.CopyTo(morePhoneNumbers, 0);
+
+                this.names = moreNames;
+                this.phoneNumbers = morePhoneNumbers;
+            }
+        }
+    }
 }
